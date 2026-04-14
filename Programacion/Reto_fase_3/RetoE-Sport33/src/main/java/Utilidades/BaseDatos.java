f1a924f35d01d7a77f234ea2ca2c45d38e55a054
Fatima <fatima.din.51227@egibide.org>
2026-04-14 12:04:02 +0200

    Cambio y actualizado base de datos

--- Programacion/Reto_fase_3/RetoE-Sport33/src/main/java/Utilidades/BaseDatos.java
+++ Programacion/Reto_fase_3/RetoE-Sport33/src/main/java/Utilidades/BaseDatos.java
@@ -1,8 +1,29 @@
 package Utilidades;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class BaseDatos {
+    private static final String URL = "jdbc:mysql://localhost:3306/bdesport";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+    private static Connection conn = null;
 
+    public static Connection getConnection()  throws Exception{
+        conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        return conn;
+    }
 
+    public static void closeConnection() throws SQLException {
+            conn.close();
+        }
 
-
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
