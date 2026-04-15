05d52de2de199c279e2cffac1717ec1107e10165
unknown <141GA07@ARRIAGAINFO.LOCAL>
2026-04-15 12:13:57 +0200

    documentacion

--- Programacion/Reto_fase_3/RetoE-Sport33/src/main/java/Modelo/Partido.java
+++ Programacion/Reto_fase_3/RetoE-Sport33/src/main/java/Modelo/Partido.java
@@ -1,5 +1,11 @@
 package Modelo;
 
+/**
+ * Calse para crear partidos con sus atributos
+ * @author Unax
+ * @version 1.0
+ * @since 2026-04-15
+ */
 public class Partido {
     public Partido(String hora, Equipo local, Equipo visitante) {
     }
