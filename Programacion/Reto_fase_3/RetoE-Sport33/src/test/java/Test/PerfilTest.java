abdfac3f319fad411a5c169b27e591a9cbc2fe1d
urko <urko.lopezdemunain.44772@ikasle.egibide.org>
2026-04-19 20:58:04 +0200

    test de pruebas

--- Programacion/Reto_fase_3/RetoE-Sport33/src/test/java/Test/PerfilTest.java
+++ Programacion/Reto_fase_3/RetoE-Sport33/src/test/java/Test/PerfilTest.java
@@ -9,21 +9,12 @@ class PerfilTest {
     @Test
     void testCrearPerfil() {
 
-        // objeto de prueba
-        Perfil p = new Perfil(1, "admin", "1234", "ADMIN");
+        Perfil p = new Perfil(1, "Eathan", "1234", "ADMIN");
 
 
         assertEquals(1, p.getCodPerfil());
-        assertEquals("admin", p.getNombre());
+        assertEquals("Eathan", p.getNombre());
         assertEquals("1234", p.getPassword());
         assertEquals("ADMIN", p.getTipo());
     }
-
-    @Test
-    void testSetters() {
-        Perfil p = new Perfil(0, "", "", "");
-        p.setNombre("Urko");
-        assertEquals("Urko", p.getNombre());
-    }
-
 }
